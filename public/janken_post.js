2d2b4f21a8926e28e60f1d0e37e19e1bb5d7b5d8
Sousi Omine <110832262+SousiOmine@users.noreply.github.com>
2024-12-03 10:55:29 +0900

    10回目講義で作成途中になったPOSTメソッド版jyankenのファイルを追加

---
+++ public/janken_post.js
@@ -0,0 +1,8 @@
+const client_hand = document.querySelector("#hand");
+const cpu_hand_dom = document.querySelector("#cpu_hand");
+
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", () => {
+    console.log("クリックしました");
+});
